3e7e178d6c22b06a30c0c555ad7d24ed6f5b4fed
MasterK <keshavagarwal12325@gmail.com>
2023-04-03 22:16:45 +0530

    added code files

---
+++ index.js
@@ -0,0 +1,13 @@
+var typeData=new Typed(".role", {
+    strings: [
+        "Front-End Developer",
+        "Android Developer",
+        "Content Writer",
+        "Programmer",
+        "Aspiring Cryptologist",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+});
---
+++ index2.js
@@ -0,0 +1,10 @@
+var typeData=new Typed(".quote", {
+    strings: [
+        "Who Am I ? And what I am up for ?",
+        "A passionate developer, who loves to explore different domains of technology. I am deeply interested in stuffs related to Ethical hacking and wanna pursue Cryptography in future. Beside this works related to Web Development and Software development thrills me the most. Want to explore more in the field of Web Development, specially the security part.",
+    ],
+    loop: true,
+    typeSpeed: 150,
+    backSpeed: 80,
+    backDelay: 1000,
+});
